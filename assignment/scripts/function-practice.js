34f0b75868ef863632443ec9f46d064447ef2626
Nolen Lawton <mnlawton@icloud.com>
2023-01-02 15:54:33 -0600

    checked first letter of a string

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -46,9 +46,9 @@ function isPositive(number) {
 }
 // Call the function to test each outcome (true & false) 
 // Write a separate console.log statement for each outcome
-console.log( /*'isPositive - should say true',*/ '3 is positive, ' + isPositive(3) );
-console.log( /*'isPositive - should say false',*/ '0 is positive, ' + isPositive(0) );
-console.log( /*'isPositive - should say false',*/ '-3 is positive, ' + isPositive(-3) );
+console.log( '3 is positive, ' + isPositive(3) );
+console.log( '0 is positive, ' + isPositive(0) );
+console.log( '-3 is positive, ' + isPositive(-3) );
 
 
 // 6. Function to return the _last_ item in an array. If the 
@@ -95,10 +95,15 @@ console.log(find(8));
 // 8. Function to check if a letter is the first letter in a 
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
-
+  if(letter === string.charAt(0)){
+      return true
+  }
+  else {
+    return false
+  }
 }
-console.log( 'isFirstLetter - should say true', isFirstLetter('a', 'apple') );
-console.log( 'isFirstLetter - should say false', isFirstLetter('z', 'apple') );
+console.log( 'the first letter of apple is a:', isFirstLetter('a', 'apple') );
+console.log( 'the first letter of apple is z:', isFirstLetter('z', 'apple') );
 
 // 9. Function to return the sum of all numbers in an array
 function sumAll( ) {
